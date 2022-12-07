a4883e0c649bebdf94a91b43348261ecde7ef7b3
bau-steinchen <mikko.ristein@gmail.com>
2022-12-07 20:36:09 +0100

    adding question for user of .oh-my-zsh

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 # change owner of .ohmyzsh to user
+cd ~/.oh-my-zsh
+if [ -n "$(find . -user "$username" -print -prune -o -prune)" ]; then
+  echo "The current directory is owned by $username."
+fi
 echo "change folder .oh-my-zsh/ to $USER!"
 sudo chown -R $USER .oh-my-zsh
 
-# clone temporary the repo for powerlevel10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 # backup
 mkdir ~/dotfiles_backup 
-mv ~/.bash_profile ~/.gitconfig ~/.oh-my-zsh/custom/themes/* ~/dotfiles_backup
+mv ~/.bash_profile ~/.gitconfig ~/.oh-my-zsh/custom/themes/* ~/.zshrc ~/dotfiles_backup
+
+# clone temporary the repo for powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # make symlinks
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
